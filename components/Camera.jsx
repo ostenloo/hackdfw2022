01de0650b61251544aed234ef41111279965c189
myusernamesthisreal <myusernamesthis@gmail.com>
2022-09-23 22:01:37 -0500

    feat: View video

--- components/Camera.jsx
+++ components/Camera.jsx
@@ -1,30 +1,47 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const Camera = () => {
-    const [mediaRecorder, setMediaRecorder] = useState(null);
     const [cameras, setCameras] = useState([]);
+    const [mics, setMics] = useState([]);
 
-    const getDevices = async() => {
+    const videoRef = useRef(null);
+
+    const getDevices = async () => {
         const devices = await navigator.mediaDevices.enumerateDevices();
         const cameras = devices.filter(device => device.kind === "videoinput");
         console.log(cameras);
-        return cameras;
+        const mics = devices.filter(device => device.kind === "audioinput");
+        console.log(mics);
+        setCameras(cameras);
+        setMics(mics);
+        return;
     }
 
     useEffect(() => {
         if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-            navigator.mediaDevices.getUserMedia({ video: true })
+            navigator.mediaDevices.getUserMedia({ video: true, audio: true })
                 .then(stream => {
-                    const mediaRecorder = new MediaRecorder(stream);
-                    setMediaRecorder(mediaRecorder);
+                    let video = videoRef.current;
+                    video.srcObject = stream;
+                    video.play();
                 })
                 .catch(err => console.error(err));
-            setCameras(getDevices());
+            getDevices();
         }
-        
+
     }, []);
+
     return (
-        <div className="camera">testing</div>
+        <>
+            <h1 className="text-5xl mb-4">Welcome to SafeStream</h1>
+            <div className="max-w-[1280px]">
+                <video ref={videoRef} autoPlay muted></video>
+            </div>
+            <div className="max-w-[1250px]">
+                <h1 className="text-3xl mt-4 overflow-hidden">THE APP IS CURRENTLY RECORDING YOUR CAMERA AND MICROPHONE. PRESS THE BUTTON ONLY WHEN THREAT IS NO LONGER PRESENT.</h1>
+            </div>
+            <button className="bg-red-500 text-white text-3xl p-4 mt-4 rounded-lg">STOP</button>
+        </>
     );
 }
 
