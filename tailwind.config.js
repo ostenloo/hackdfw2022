e147ecaccfad99c509af34e5594bb91db088b8c2
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-09-24 06:42:17 -0500

    [feat] Color Scheme, Button Toggle
    - Added New Color Schema (See Discord)
    - Implemented Button Toggle Over Display

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { 
+        google : ['Roboto,arial,sans-serif'],
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'spacecadet': '#1D3461',
+        'yaleblue': '#1F487E',
+        'queenblue': '#376996',
+        'unblue': '#6290C8',
+        'ceruleanfrost': '#829CBC',
+      },
+    },
   },
   plugins: [],
 }
